d5eb30937d5a18aa602efcdad8dd62a795d3fb3a
Douglas-Andrade-Severa <douglas.severa96@gmail.com>
2023-11-26 14:30:08 -0300

    ajuste

--- alertablu.py
+++ alertablu.py
@@ -1,24 +1,23 @@
 import requests
-from bs4 import BeautifulSoup #pip install beautifulsoup
+from bs4 import BeautifulSoup 
 from io import StringIO
 import pandas as pd
 
-url = 'https://alertablu.blumenau.sc.gov.br/d/'
-colunas = ['Estação','Hora da leitura','24h','168h']
-arquivo = 'alertablu.cvs'
+url = 'https://www.alertablu.sc.gov.br/d/'
+colunas = ['Estação', 'Hora da Leitura', '24h', '168h']
+arquivo = 'alertablu.csv'
 
 def main():
     try:
         response = requests.get(url, verify=False)
-        response.raise_for_status()
+        response.rause_for_status()
 
-        soup  = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
         table = soup.find('table')
         table = StringIO(str(table))
 
         df = pd.read_html(table, thousands='.', decimal=',')[0]
         df = df[colunas]
-
         df.to_csv(arquivo, header=False, index=False, mode='a')
 
     except Exception as e:
--- app.py
+++ app.py
@@ -1,119 +1,71 @@
-import dash 
+import dash
 from dash import dcc, html
-from dash import Input, Output, State
 import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 import pandas as pd
-import yfinance as yf
+import preprocessamento
+import graficos
 
-#ler e ajustar os dados
+# Inicialização da aplicação
 
-#acoes_b3 = ['PETR4.SA',#PETROBRAS
-#            'VALE3.SA',#VALE
-#            'ITUB4.SA',#ITAU
-#            'BBDC4.SA',#BB
-#            'ABEV3.SA',#AMBEV
-#            'B3SA3.SA',#B3
-#            'MRFG3.SA',#MARFRIG
-#            'WEGE3.SA',#WEG
-#            'SUZB3.SA',#SUZANO
-#            'VIVT3.SA',#VIVO
-#            '^BVSP']   #INDICE IBOVESPA
-
-
-#df = yf.download(acoes_b3, period='1y')
-#df.index.name = None
-#df.to_csv('b3.csv')
-data = pd.read_csv('b3.csv', index_col=0)
-
-#Contrução do gráfico
-
-data2 = data['VALE3.SA']
-fig2  = px.line(data2)
-fig2.update_layout(title='VALE3.SA',
-                   yaxis_title='Preço (R$)',
-                   xaxis_title='Data',
-                   template='plotly_dark',
-                   showlegend=False) #plotly.com/python/templates
-
-data3 = data['ITUB4.SA']
-fig3  = px.line(data3)
-fig3.update_layout(title='ITUB4.SA',
-                   yaxis_title='Preço (R$)',
-                   xaxis_title='Data',
-                   template='plotly_dark',
-                   showlegend=False) #plotly.com/python/templates
-
-
-data4 = pd.read_csv('petro.csv', index_col=0)
-candle = go.Candlestick(x=data4.index,
-                        open=data4['Open'],
-                        high=data4['High'],
-                        low=data4['Low'],
-                        close=data4['Close'])
-
-fig4 = go.Figure(data=candle)
-fig4.update_layout(title='Ações da petrobras',
-                   yaxis_title='Preço (R$)',
-                   xaxis_title='Data',
-                   template='plotly_dark') #plotly.com/python/templates
-
-#Inicialização da aplicação
-app =  dash.Dash(__name__,
-                 external_stylesheets=[dbc.themes.CYBORG]) #bootswatch.com/
+app = dash.Dash(__name__, 
+                external_stylesheets=[dbc.themes.CYBORG])
 
 #Layout do dashboard
-app.layout = dbc.Container([#dash-bootstrap-components.opensource.faculty.ia/docs/components
-    dbc.Row([
+
+app.layout = dbc.Container([
+
+    dbc.Row([ #Primeira linha
         dbc.Col([
-            html.H1('Dashboard de Mercado Financeiro',
-                    className='text-center text-primary'),# dash.plotly.com/dash-html-components
+            html.H1('Dashboard Monitoramento de Chuvas',
+                    className= 'text-center text-primary'),
             html.Br()
         ])
     ]),
 
-    dbc.Row([
+    dbc.Row([ #Segunda linha
         dbc.Col([
-            dcc.Dropdown(id='menu', value=['ABEV3.SA'],
-                        options={x:x for x in data.columns},
-                        multi=True),
-            dcc.Graph(id='grafico')
+            dcc.Graph(figure=graficos.figura1)
         ])
     ]),
 
-    dbc.Row([
+    dbc.Row([ #Terceira linha
+        dbc.Col([ #Primeira coluna
+              dcc.Dropdown(id='menu', value=['Glória','Velha Central', 'Itoupavazinha','Vorstadt'],
+                           multi=True, options=[{'label':x, 'value':x} for x in graficos.pluvio_estacoes.columns]),
+              dcc.Graph(id='grafico')
+        ], width=6),
+        dbc.Col([ #Segunda coluna
+            dcc.Graph(figure=graficos.figura3)
+        ], width={'size': 6})
+    ], class_name='g-0'),
+
+    dbc.Row([ #Quarta linha
         dbc.Col([
-            dcc.Graph(figure=fig2)
-        ], width=4),
-        dbc.Col([
-            dcc.Graph(figure=fig3)
-        ], width={'size':'8'})
-    ], className='g-0'),
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(figure=fig4)
+            dcc.Graph(figure=graficos.figura3)
         ])
     ])
 ])
 
-
 # Callbacks
 @app.callback(
-    Output('grafico','figure'),
+    Output('grafico', 'figure'),
     Input('menu', 'value')
 )
-def funcao(acoes):
-    data_fig = data[acoes]
-    fig  = px.line(data_fig)
-    fig.update_layout(title='B3 10+',
-                    yaxis_title='Preço (R$)',
-                    xaxis_title='Data',
-                    template='plotly_dark',
-                    showlegend=False) #plotly.com/python/templates
-    return fig
-    
 
-#Execução da aplicação
+def funcao(acoes):
+    data_fig = px.line(graficos.pluvio_estacoes[acoes])
+    fig = (data_fig)
+    fig.update_layout(title='Índice Pluviométrico por Estação',
+                        yaxis_title='',
+                        xaxis_title='',
+                        template='plotly_dark',
+                        showlegend=False)
+    return fig
+
+
+# Execução do aplicação
 if __name__ == '__main__':
     app.run_server(debug=True)
